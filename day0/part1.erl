4df8192e2d9365c88aa386c6ade595772c9cd67d
Laurie Hedge <lrhedge@gmail.com>
2023-11-29 22:26:39 +0000

    Setup repo and template

---
+++ day0/part1.erl
@@ -0,0 +1,5 @@
+-module(part1).
+-export([start/0]).
+
+start() ->
+	io:format("Hello from part 1!~n").
---
+++ day0/part2.erl
@@ -0,0 +1,5 @@
+-module(part2).
+-export([start/0]).
+
+start() ->
+	io:format("Hello from part 2!~n").
