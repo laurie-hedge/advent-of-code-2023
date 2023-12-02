b6655ea6795247606ce274910d9ce33a830c7673
Laurie Hedge <lrhedge@gmail.com>
2023-12-02 08:14:04 +0000

    Day 2

---
+++ day2/part2.erl
@@ -0,0 +1,38 @@
+-module(part2).
+-export([start/0]).
+
+start() ->
+	Lines = read_file_lines("day2/input.txt"),
+	Result = process_lines(Lines),
+	io:format("Result: ~w~n", [Result]).
+
+read_file_lines(File_Name) ->
+	{ok, Data} = file:read_file(File_Name),
+	Chars = lists:flatten(binary_to_list(Data)),
+	string:tokens(Chars, "\n").
+
+process_lines(Lines) ->
+	process_lines(Lines, 0).
+process_lines([], Sum_Of_Powers) ->
+	Sum_Of_Powers;
+process_lines([Line|Lines], Sum_Of_Powers) ->
+	[_, Results_Part_Str] = string:tokens(Line, ":"),
+	Results_Parts = string:tokens(Results_Part_Str, " ,;"),
+	process_lines(Lines, new_sum_of_powers(Sum_Of_Powers, get_max_cubes_by_colour(Results_Parts))).
+
+get_max_cubes_by_colour(Results_Parts) ->
+	get_max_cubes_by_colour(Results_Parts, {0, 0, 0}).
+get_max_cubes_by_colour([], Current_Max) ->
+	Current_Max;
+get_max_cubes_by_colour([Num_Cubes_Str, Cube_Colour|Rest], Current_Max) ->
+	{Num_Cubes, _} = string:to_integer(Num_Cubes_Str),
+	{Max_Red, Max_Green, Max_Blue} = Current_Max,
+	New_Max = case Cube_Colour of
+		"red" -> { lists:max([Max_Red, Num_Cubes]), Max_Green, Max_Blue };
+		"green" -> { Max_Red, lists:max([Max_Green, Num_Cubes]), Max_Blue };
+		"blue" -> { Max_Red, Max_Green, lists:max([Max_Blue, Num_Cubes]) }
+	end,
+	get_max_cubes_by_colour(Rest, New_Max).
+
+new_sum_of_powers(Sum_Of_Powers, {Max_Red, Max_Green, Max_Blue}) ->
+	Sum_Of_Powers + (Max_Red * Max_Green * Max_Blue).
